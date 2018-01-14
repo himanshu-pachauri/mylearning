af04805e54b0533b040a7e0e1713a21fa2bea674
Himanshu Pachauri <hpachauri786@grid-scape.com>
2018-01-14 15:01:06 +0530

    first commit

---
+++ UNIX_IO/read.c
@@ -0,0 +1,24 @@
+//gcc read.c -o read 
+#include<stdio.h>
+#include<unistd.h>
+
+int main()
+{
+char buf[1024];
+ssize_t byteread;
+byteread=read(STDIN_FILENO,buf,100);
+
+if(byteread<100)
+{
+printf("Error in opening");
+return 0;
+}
+else 
+printf(" you have entered %s",buf);
+return 0;
+
+
+
+
+
+}
